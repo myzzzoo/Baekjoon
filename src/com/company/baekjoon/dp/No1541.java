c243887989d1c431ace446285edba3cbef29f7c9
Heonjun Jeong <gjswns1991@gmail.com>
2021-08-19 13:14:45 +0900

    s2 잃어버린 괄호

---
+++ src/com/company/baekjoon/dp/No1541.java
@@ -0,0 +1,32 @@
+package com.company.baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+//잃어버린 괄호
+public class No1541 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String[] minus = s.split("-");
+        int sum = 0;
+
+        for(int i=0; i<minus.length; i++){
+            String[] plus = minus[i].split("\\+");
+            int subSum = 0;
+            for(int j=0; j<plus.length; j++){
+                int num = Integer.parseInt(plus[j]);
+                subSum+=num;
+            }
+            if(i==0){
+                sum+=subSum;
+            }
+            else{
+                sum-=subSum;
+            }
+        }
+        System.out.println(sum);
+    }
+}
