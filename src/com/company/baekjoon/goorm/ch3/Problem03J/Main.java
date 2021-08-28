f89cf4a566d78b6e07945c48b673e60e90446d47
Heonjun Jeong <gjswns1991@gmail.com>
2021-08-28 10:21:04 +0900

    공간활용하기/네카드

--- src/com/company/baekjoon/goorm/ch3/Problem03J/Main.java
+++ src/com/company/baekjoon/goorm/ch3/Problem03J/Main.java
@@ -1,19 +1,15 @@
-package com.company.baekjoon.goorm.ch3.Problem03I;
+package com.company.baekjoon.goorm.ch3.Problem03J;
 
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 /**
- * 세카드
+ * 네 카드
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] s = br.readLine().split(" ");
         String[] s2 = br.readLine().split(" ");
@@ -27,39 +23,50 @@ public class Main {
         for(int i=0; i<n; i++){
             cards[i] = Integer.parseInt(s2[i]);
         }
-
         for(int i=0; i<m; i++){
             targets[i] = Integer.parseInt(s3[i]);
         }
-
         Arrays.sort(cards);
 
+
+        HashSet<Integer> pairSetCards = new HashSet<>();
+        for(int i=0; i<n; i++){
+            for (int j=0; j<n; j++) {
+                pairSetCards.add(cards[i]+cards[j]);
+            }
+        }
+
+        int[] pairCards = new int[pairSetCards.size()];
+        int idx=0;
+        Iterator it = pairSetCards.iterator();
+        while(it.hasNext()){
+            pairCards[idx]=Integer.parseInt(String.valueOf(it.next()));
+            idx++;
+        }
+        Arrays.sort(pairCards);
         ArrayList<Integer> result = new ArrayList<>();
         for(int i=0; i<m; i++){
             boolean possible = false;
-            for(int j=0; j<n; j++){
-                int x = cards[j];
-                for(int k=0; k<n; k++){
-                    int y = cards[k];
-                    int z = targets[i]-x-y;
-                    if(Arrays.binarySearch(cards,z)>=0) {
-                        possible = true;
-                        break;
-                    }
-                }
-                if(possible==true)
+            for(int j=0; j< pairCards.length; j++){
+                if(Arrays.binarySearch(pairCards,targets[i]-pairCards[j])>=0){
+                    possible = true;
                     break;
+                }
             }
-            if(possible==true)
+            if(possible)
                 result.add(targets[i]);
         }
+
         Collections.sort(result);
         if(result.isEmpty())
             System.out.println("NO");
-        else {
+        else{
             for(int i:result){
                 System.out.print(i+" ");
             }
         }
+
+
+
     }
 }
