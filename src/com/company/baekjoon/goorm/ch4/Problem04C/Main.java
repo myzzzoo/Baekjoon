b6ccec3f783c3af0aeeb87b49f444ab4c3134192
Heonjun Jeong <gjswns1991@gmail.com>
2021-08-30 15:24:47 +0900

    자주사용되는수학알고리즘/GCD&LCM

---
+++ src/com/company/baekjoon/goorm/ch4/Problem04C/Main.java
@@ -0,0 +1,33 @@
+package com.company.baekjoon.goorm.ch4.Problem04C;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 최대공약수와 최소공배수
+ */
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<tc; i++){
+            String[] input = br.readLine().split(" ");
+            int numA = Math.max(Integer.parseInt(input[0]),Integer.parseInt(input[1]));
+            int numB = Math.min(Integer.parseInt(input[0]),Integer.parseInt(input[1]));
+
+            long a = numA;
+            long b = numB;
+            while(a%b!=0){
+                long c = a%b;
+                a = b;
+                b = c;
+            }
+            long gcd = b;
+            long lcm = (numB/gcd)*numA;
+            System.out.println("Case #"+(i+1)+":");
+            System.out.println(gcd+" "+lcm);
+        }
+    }
+}
