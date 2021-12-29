45ef484161df14a62b8e270628bd5d562251a89f
Heonjun Jeong <gjswns1991@gmail.com>
2021-12-29 09:23:32 +0900

    1로만들기

--- src/com/company/baekjoon/dp/No1463.java
+++ src/com/company/baekjoon/dp/No1463.java
@@ -7,35 +7,30 @@ import java.io.InputStreamReader;
 /////1로 만들기
 public class No1463 {
 
-    static int[] arr;
-
-    static int DP(int N){
-        if(N==1)
-            return 0;
-        if(arr[N]>0)
-            return arr[N];
-        arr[N] = DP(N-1)+1;
-
-        if(N%2 ==0){
-            arr[N] = Math.min(DP(N/2),DP(N-1))+1;
-        }
-
-        if(N%3 ==0){
-            arr[N] = Math.min(DP(N/3),DP(N-1))+1;
-        }
-
-
-        return arr[N];
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        arr= new int[N+1];
+        int[] arr= new int[N+1];
 
+        arr[1] = 0;
+        if(N>1)
+            arr[2] = 1;
+        if(N>2)
+            arr[3] = 1;
 
-        System.out.println(DP(N));
+        for(int i=4; i<=N; i++){
+            if(i%6==0)
+                arr[i] = Math.min(Math.min(arr[i/3],arr[i/2]),arr[i-1])+1;
+            else if(i%3==0)
+                arr[i] = Math.min(arr[i/3],arr[i-1])+1;
+            else if(i%2==0)
+                arr[i] = Math.min(arr[i/2],arr[i-1])+1;
+            else
+                arr[i] = arr[i-1]+1;
+        }
+
+        System.out.println(arr[N]);
 
     }
 
