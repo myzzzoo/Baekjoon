ce5a435a38f690a146028b32df90998717a61b6a
Heonjun Jeong <gjswns1991@gmail.com>
2022-01-26 17:15:21 +0900

    다익스트라(힙)

--- src/com/company/baekjoon/shortestpath/Dijkstra.java
+++ src/com/company/baekjoon/shortestpath/Dijkstra.java
@@ -30,7 +30,7 @@ class Node implements Comparable<Node> {
     }
 }
 
-public class dijkstra {
+public class Dijkstra {
 
     public static final int INF = (int) 1e9; // 무한을 의미하는 값으로 10억을 설정
     // 노드의 개수(N), 간선의 개수(M), 시작 노드 번호(Start)
